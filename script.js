2f8f112b9d6f70895b26a9b7049c270b3e4648ac
Sai2507 <113433156+Sai2507@users.noreply.github.com>
2024-05-25 21:55:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger');
-    const navLinks = document.querySelector('.nav-links');
 
-    burger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        burger.classList.toggle('toggle');
-    });
-});
 // document.addEventListener("contextmenu", function(event){
 //     alert("This Functionality is Disabled By Default... Please Contact Admin")
 //     event.preventDefault();
 // });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Function to hide the navbar
+    const hideNavbar = () => {
+        navLinks.classList.remove('active');
+        burger.classList.remove('toggle');
+    };
+
+    // Toggle navbar on burger click
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        burger.classList.toggle('toggle');
+    });
+
+    // Hide navbar on scroll
+    window.addEventListener('scroll', hideNavbar);
+
+    // Hide navbar on page load
+    window.addEventListener('load', hideNavbar);
+});
